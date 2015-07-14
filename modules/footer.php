e0856a0c344fef40be87752760f59d378338f856
Joe Cramer <cramerjs@gmail.com>
2015-07-13 21:37:36 -0600

    modified footer css, plus house cleaning

--- modules/footer.php
+++ modules/footer.php
@@ -3,15 +3,23 @@
  *  FOOTER MODULE for edcarchitect.com
  */
 ?>
-    <footer>
-        <ul>
-            <li><a href="index.php?action=site_plan">Site Plan</a></li>
-            <li><p>Elvin Dee Cramer Architect &copy; <?php echo date("Y"); ?></p></li>
-            <li><p>702-349-3551</p></li>
-            <li><p>elvin@edcarchitect.com</p></li>
-        </ul>
-    </footer>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-</body>
+        <footer class="footer">
+            <a href="index.php?action=site_plan">Site Plan</a>
+            <p>Elvin Dee Cramer Architect &copy; <?php echo date("Y"); ?></p>
+            <p>702-349-3551</p>
+            <p>elvin@edcarchitect.com</p>
+            <!--
+                <ul>
+                    <li><a href="index.php?action=site_plan">Site Plan</a></li>
+                    <li><p>Elvin Dee Cramer Architect &copy; <?php echo date("Y"); ?></p></li>
+                    <li><p>702-349-3551</p></li>
+                    <li><p>elvin@edcarchitect.com</p></li>
+                </ul>
+            -->
+        </footer>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+    </body>
 </html>
